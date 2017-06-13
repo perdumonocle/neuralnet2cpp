edc74e497650d1b3255c2278dafa207845b8ff72
perdumonocle <konstantin_1987@mail.ru>
2017-06-13 06:49:19 +0500

    Create compute.gen.cpp.r

---
+++ compute.gen.cpp.r
@@ -0,0 +1,100 @@
+compute.gen.cpp <-
+function (x, path, rep = 1, float.type = "double", namespace = TRUE) 
+{
+    fd = file( path, open = "wt" );
+
+    nn <- x
+    linear.output <- nn$linear.output
+    weights <- nn$weights[[rep]]
+    nrow.weights <- sapply( weights, nrow )
+    ncol.weights <- sapply( weights, ncol )
+    length.weights <- length( weights )
+    num.covariates <- nrow.weights[1] - 1
+
+    act.fct.type <- attributes( net$act.fct )$type
+    if ( act.fct.type == "logistic" )
+    {
+        cat( "#include <math.h>\n", file = fd )
+        cat( "\n", file = fd )
+    }
+
+    if ( namespace )
+    {
+        cat( "namespace neuralnet\n", file = fd )
+        cat( "{\n", file = fd )
+        cat( "\n", file = fd )
+    }
+
+    if ( act.fct.type == "logistic" )
+    {
+        cat( "static ", float.type, " act_fct(", float.type, " x)\n", sep = "", file = fd )
+        cat( "{\n", file = fd )
+        cat( "    return 1 / ( 1 + exp(-x) );\n", file = fd )
+        cat( "}\n", file = fd )
+        cat( "\n", file = fd )
+    }
+
+    cat( "void compute(const ", float.type, " *covariates, ", float.type, " *results)\n", sep = "", file = fd )
+    cat( "{\n", file = fd )
+
+    if ( num.covariates > 1 )
+    {
+        for ( i in 0:(num.covariates - 2) )
+        {
+            cat( "    const ", float.type, " n0_", i, " = *(covariates++);\n", sep = "", file = fd )
+        }
+    }
+    cat( "    const ", float.type, " n0_", (num.covariates - 1), " = *(covariates);\n", sep = "", file = fd )
+    cat( "\n", file = fd )
+
+    for ( l in 1:length.weights )
+    {
+        layer.weights <- weights[[l]]
+        layer.intercepts <- layer.weights[1,]
+
+        use.act.fct = "act_fct"
+        if ( l == length.weights && linear.output )
+        {
+            use.act.fct = ""
+        }
+
+        for ( n in 1:(ncol.weights[l]) )
+        {
+            intercept <- layer.intercepts[n]
+            multsum <- paste( "(", layer.weights[2, n], ") * n", (l - 1), "_0", sep = "" )
+            if ( nrow.weights[l] > 2 )
+            {
+                for ( i in 3:(nrow.weights[l]) )
+                {
+                    multsum <- paste( multsum, " + (", layer.weights[i, n], ") * n", (l - 1), "_", (i - 2), sep = "" )
+                }
+            }
+            if ( l == length.weights )
+            {
+                if ( n == ncol.weights[l] )
+                {
+                    cat( "    *results = ", use.act.fct, "( ", intercept, " + ", multsum, " );", sep = "", file = fd )
+                }
+                else
+                {
+                    cat( "    *(results++) = ", use.act.fct, "( ", intercept, " + ", multsum, " );\n", sep = "", file = fd )
+                }
+            }
+            else
+            {
+                cat( "    const ", float.type, " n", l, "_", (n - 1), " = ", use.act.fct, "( ", intercept, " + ", multsum, " );\n", sep = "", file = fd )
+            }
+        }
+        cat( "\n", file = fd )
+    }
+
+    cat( "}\n", file = fd )
+    cat( "\n", file = fd )
+
+    if ( namespace )
+    {
+        cat( "} // namespace neuralnet\n", file = fd )
+    }
+
+    close( fd )
+}
